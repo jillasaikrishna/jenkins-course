bd7ee41e39337195f534f13b1e2716f743394f2c
saikrishna_nutanix <jillasai.krishna@nutanix.com>
2021-07-17 23:01:43 +0530

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -14,7 +14,7 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('krishnasdocker789/jilla_docker-nodejs-demo)
+            repositoryName('krishnasdocker789/jilla_docker-nodejs-demo')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('docker@9441710946')
             forcePull(false)
